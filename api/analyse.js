9c8771b83b18d0fd9b07b74ba5b57812c7e1678b
romeu-claro-app <romeu.m@outlook.pt>
2026-05-03 11:51:49 +0200

    Update analyse.js

--- api/analyse.js
+++ api/analyse.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
