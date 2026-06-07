70af509bd991e1af600b6a50599018dae967aa0d
Romeu Martins <romeu.m@outlook.pt>
2026-06-07 08:53:18 +0200

    fix: aumentar timeout análise para 60s

--- api/analyse.js
+++ api/analyse.js
@@ -1,3 +1,5 @@
+module.exports.config = { maxDuration: 60 };
+
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
